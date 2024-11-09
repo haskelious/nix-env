2349296e26589578d857246a854779742f6368eb
Stephane <stephane@frozenkiwi.net>
2024-11-09 22:17:38 +0100

    standardize around docker (instead of podman)

--- build.sh
+++ build.sh
@@ -1,16 +1,13 @@
 #! /usr/bin/env bash
 
-# use podman in place of docker
-docker=podman
-
 # build nix-base docker image
 nix-build nix-base.nix && \
 
 # import image
-${docker} load -i ./result && \
+docker load -i ./result && \
 
 # build nix-env image on top of nix-base
-${docker} build -t nix-env:latest -f Dockerfile && \
+docker build -t nix-env:latest -f Dockerfile && \
 	
 # clean up nix artifact
 rm ./result
--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
 #! /usr/bin/env bash
 
-# use podman in place of docker
-podman run -ti --rm -p 8080:8080 -v ./nix:/home/nix --userns keep-id:uid=1000,gid=100 haskelious/nix-env:latest
+docker run -ti --rm -p 8080:8080 -v ./nix:/home/nix --userns keep-id:uid=1000,gid=100 haskelious/nix-env:latest
