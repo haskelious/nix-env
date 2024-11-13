a063d94628b069b33f72a2131f052c08a2e86ff2
Stephane <stephane@frozenkiwi.net>
2024-11-13 23:51:32 +0100

    fix rust compilation with cargo

--- envs/rust.nix
+++ envs/rust.nix
@@ -15,6 +15,13 @@ pkgs.mkShell {
 
   shellHook = ''
     #! /usr/bin/env bash
+
+    # see https://nixos.wiki/wiki/Development_environment_with_nix-shell#Troubleshooting
+    export NIX_ENFORCE_PURITY=0
+
+    code-server --install-extension rust-lang.rust-analyzer && \
+    ln -sf /nix/store/*-rust-analyzer-*/bin/rust-analyzer ~/.local/share/code-server/extensions/rust-lang.rust-analyzer-*/server/
+
     (sleep 2; cat ~/.config/code-server/config.yaml) &
     code-server
   '';
