bb15ed06624afc0271b21a6a38e99fc09249cbd3
Stephane <stephane@frozenkiwi.net>
2024-11-16 23:37:19 +0100

    minor enhancement on rust script

--- envs/rust.nix
+++ envs/rust.nix
@@ -13,13 +13,16 @@ pkgs.mkShell {
 
   RUST_SRC_PATH="${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
 
+  # see https://nixos.wiki/wiki/Development_environment_with_nix-shell#Troubleshooting
+  NIX_ENFORCE_PURITY=0
+
   shellHook = ''
     #! /usr/bin/env bash
 
-    # see https://nixos.wiki/wiki/Development_environment_with_nix-shell#Troubleshooting
-    export NIX_ENFORCE_PURITY=0
-
+    # install code-server extension
     code-server --install-extension rust-lang.rust-analyzer && \
+
+    # replace the rust-analyzer binary in the code-server extension folder
     ln -sf /nix/store/*-rust-analyzer-*/bin/rust-analyzer ~/.local/share/code-server/extensions/rust-lang.rust-analyzer-*/server/
 
     (sleep 2; cat ~/.config/code-server/config.yaml) &
